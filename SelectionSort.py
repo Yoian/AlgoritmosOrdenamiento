e76e3f50c6372b373eb53a5db6bbdf31cfd0c722
Ian Oliva <yoian@outlook.com>
2023-02-19 18:18:00 -0600

    Adding the Selection Sort Algorithm

---
+++ SelectionSort.py
@@ -0,0 +1,18 @@
+from random import randrange
+
+def selectionSort(array):
+    for i in range(len(array)):
+
+        idxDes = i
+        for j in range(i+1,len(array)):
+            if array[idxDes]>array[j]:
+                idxDes = j
+
+        array[i],array[idxDes]=array[idxDes],array[i]
+
+
+lista = [randrange(1,300,8) for i in range(10)]
+
+print("La lista desordenada se ve asi",lista)
+selectionSort(lista)
+print("La lista ordenada se ve asi",lista)
